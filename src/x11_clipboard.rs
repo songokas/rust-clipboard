341ffd6f28fd85a5af3d7edbdc5ac8c1d0c2e6f8
Tomas Jakstas <tom.jakstas@gmail.com>
2025-01-13 16:00:59 +0200

    github actions for linux 2

--- src/x11_clipboard.rs
+++ src/x11_clipboard.rs
@@ -371,6 +371,9 @@ mod tests {
     #[serial_test::serial]
     #[test]
     fn test_wait_for_target_and_obtain_other_targets() {
+        // random connection reset
+        std::thread::sleep(Duration::from_millis(100));
+
         let poll_duration = Duration::from_secs(1);
         let c1 = b"yes plain";
         let c2 = b"yes html";
