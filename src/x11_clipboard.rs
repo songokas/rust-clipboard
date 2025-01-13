d915aad8244c2e2d02c486eb834574e29c708180
Tomas Jakstas <tom.jakstas@gmail.com>
2025-01-13 22:46:25 +0200

    linux tests

--- src/x11_clipboard.rs
+++ src/x11_clipboard.rs
@@ -405,6 +405,7 @@ mod tests {
             std::thread::sleep(Duration::from_millis(500));
         });
 
+        std::thread::sleep(Duration::from_millis(100));
         let mut context = ClipboardContext::new().unwrap();
 
         let t2 = std::thread::spawn(move || {
